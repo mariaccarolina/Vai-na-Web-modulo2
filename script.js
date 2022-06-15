54f9c34077cc43698ebdb5bb48ed74a43186406a
Maria Carolina <demycar24@gmail.com>
2022-06-15 20:50:40 -0300

    Funções JS

---
+++ script.js
@@ -0,0 +1,19 @@
+function mensagem (){
+    console.log ('Hello!!!')
+}
+mensagem ()
+
+function oi (nome){
+    console.log (nome)
+}
+oi('Maria Carolina')
+
+function informacoes (nome,idade, gostomusical){
+console.log (nome,idade, gostomusical)
+}
+informacoes ('Maria Carolina,','28,','Pop e Rock')
+
+function lazer (filme, musica){
+    console.log (filme, musica)
+}
+lazer ('it a coisa,','NXZero Espero a minha vez')
