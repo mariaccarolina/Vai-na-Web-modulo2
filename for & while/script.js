360ad651d95938d1f6fdfad526c9b3d4d50516fc
Maria Carolina <demycar24@gmail.com>
2022-07-08 19:32:42 -0300

    for & while

---
+++ for & while/script.js
@@ -0,0 +1,16 @@
+//1 - Criar um loop que conte de 1 até 10 usando FOR
+for (let i=1;i<=10; i++){
+    console.log (i)
+}
+
+
+//3 - Criar um loop que conte todos os números ímpares de 1 até 100
+for(let i = 1; i <= 100; i += 2){
+    console.log(i);
+}
+
+
+//4- Criar um loop que conte todos os números pares de 0 a 100
+for(let i = 0; i <= 100; i += 2){
+    console.log(i);
+}
